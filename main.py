74341f2a70a90edc08c5a7d7cec24e3e80d64e1a
Amin TENNAH <amin.tennah@edu.esiee.fr>
2026-01-21 19:25:33 +0100

    probleme lancement réglé

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.app import create_app
 
 def main() -> None:
     app = create_app()
-    app.run_server(debug=False, host="0.0.0.0", port=8050)
+    app.run(debug=False, host="0.0.0.0", port=8050)
 
 
 if __name__ == "__main__":
